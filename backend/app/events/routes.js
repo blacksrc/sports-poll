d758f17eb99786d85fe35d59d901cf25b1f32e89
Siamak A.Motlagh <siamak.aghaeipour@gmail.com>
2019-02-25 04:11:55 +0330

    route controller has been updated

--- backend/app/events/routes.js
+++ backend/app/events/routes.js
@@ -3,7 +3,7 @@ const EventsControllers = require("./controller");
 
 routes.post("/", EventsControllers.createEvent);
 routes.get("/", EventsControllers.fetchEvents);
-routes.get("/:id", EventsControllers.fetchEvent);
+routes.get("/:id", EventsControllers.fetchEvents);
 routes.delete("/:id", EventsControllers.deleteEvent);
 
 module.exports = routes;
